89f8ad31c86233dc401460e3dbde73bb67617cf9
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2022-03-19 10:30:53 +0900

    20220319r1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  // final parts = ['chest', 'arm', 'leg'];
+    final parts = [{"name": "chest", "path": "/chest"},
+                   {"name": "arm", "path": "/arm"},
+                   {"name": "leg", "path": "/leg"},];
 
   @override
   Widget build(BuildContext context) {
@@ -19,22 +23,35 @@ class MyApp extends StatelessWidget {
           title: Text("test tittle")
         ),
         body: GridView.count(
-          crossAxisCount: 3,
-          children: List.generate(10, (index) {
+          crossAxisCount: 2,
+          children: parts.map((e) {
             return Center(
               child: TextButton(
                 child: Text(
-                  'Item $index',
-                  style: Theme.of(context).textTheme.headline5,            
-                ),
+                e["name"] as String
+              ),
                 onPressed: () {
-
+                  Navigator.pushNamed(context, e["path"] as String);
                 },
               )
             );
-          }),
+          }).toList(),
+          // children: List.generate(10, (index) {
+          //   return Center(
+          //     child: TextButton(
+          //       child: Text(
+          //         'Item $index',
+          //         style: Theme.of(context).textTheme.headline5,            
+          //       ),
+          //       onPressed: () {
+          //         Navigator.pushNamed(context, '/next');
+          //       },
+          //     )
+          //   );
+          // }),
         ),
       ),
     );
   }
 }
+
