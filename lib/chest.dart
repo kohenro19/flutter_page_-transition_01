232c260e3f88252806538a860b97c4ca78ac1b52
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2022-03-30 18:52:44 +0900

    20220330r2

--- lib/chest.dart
+++ lib/chest.dart
@@ -11,14 +11,40 @@ class Chest extends StatelessWidget {
           title: Text("Chest Workout"),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              // 
-            ]
-          )
+        // decoration: BoxDecoration(
+        //   gradient: LinearGradient(
+        //     colors: [
+        //       // 
+        //     ]
+        //   )
+        // ),
+        child: Container(
+          height: 135,
+          width: 500,
+          color: Colors.redAccent,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/arm.jpg'
+                        )
+                      )
+                    ),
+                  )
+                ],
+              )
+            ],
+
+          ),
+          
         ),
-        child: Column(),
       ),
     
 
